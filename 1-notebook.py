4b43f0073fa9ad124dadf78365d4ba40f183f027
Adnan0906 <142591010+Adnan0906@users.noreply.github.com>
2023-09-03 17:58:51 +0530

    Add files via upload

---
+++ 1-notebook.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Data science tools
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# OBJECTIVES
+# - popular languages for datascience
+# - popular libraries used by datascientist
+# - arithmetic expressions
+# 
+
+# Some of the popular languages that Data Scientists use are:
+# Then create an ordered list (i.e. numbered) listing 3 (or more) commonly used languages for data science.
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1.numpy()
+# 2.pandas()
+# 3.matplotlib()
+
+# # Below are a few examples of evaluating arithmetic expressions in Python
+
+# x=12
+# y=22
+# print(x+y)
+
+# In[9]:
+
+
+a=15
+b=19
+print(a*b)
+
+
+# In[4]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[5]:
+
+
+#this will convert 200 minutes to hours by diving by 60
+minutes = 200
+hours = minutes / 60
+hours
+
+
+# ## AUTHOR
+# ADNAN KHAN
